32930bc7372922ff7c0a924093dd5d85d87b0537
smrsgv <samir.isagov@gmail.com>
2023-11-19 01:52:33 +0100

    Remove app module, add linter and prettier configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [RouterOutlet],
 })
-export class AppComponent {
-  title = 'medium-clone-angular';
-}
+export class AppComponent {}
